7b95cdabbaa704c4b22c4caa70c1bc8718316e41
Aleksandr Bogatov <al3.bogatov@gmail.com>
2021-09-18 15:56:26 +0300

    Session implemented + additional task completed (Don't click on the cursor)

--- js/main.js
+++ js/main.js
@@ -3,8 +3,16 @@ $(function () {
         var r;
         var chosenR;
 
+		function killSwitch() {
+			$( "div.cursor" ).replaceWith( "<video id=\"player\" controls></video>" );
+			player.src = "media/udied.mp4";
+   			player.load();
+			player.play();
+		}
+	
         window.onload = function () {
             let buttons = document.getElementsByName("rvalue");
+			let cursedCursorElms = document.getElementsByName("cursed");
             for (var i = 0; i < buttons.length; i++) {
                 buttons[i].addEventListener("click", function (event) {
                     r = this.value;
@@ -36,6 +44,11 @@ $(function () {
                     cy: 180
                 }, 2000);
             })
+			for (var i = 0; i < cursedCursorElms.length; i++) {
+				cursedCursorElms[i].addEventListener("click", function (event) {
+					killSwitch();
+				})
+			}
         }
 
         function validateNumber(number) {
@@ -82,6 +95,39 @@ $(function () {
             return validateR() && validateX() && validateY();
         }
 
+
+        $(document).ready(function () {
+            $.ajax({
+                url: "php/restoration.php",
+                async: true,
+                type: "GET",
+                success: function (response) {
+                    let table = document.getElementById("result-table");
+                    table.insertAdjacentHTML('beforeend', response);
+                }
+            })
+        })
+
+        $(document).ready(function () {
+            $('#clean').on('click', function (e) {
+                e.preventDefault();
+                console.log("here")
+                $.ajax({
+                    url: "php/erase.php",
+                    async: true,
+                    type: "GET",
+                    data: {},
+                    cache: false,
+                    success: function (response) {
+                        $('#result-table tr').slice(1).remove();
+                    },
+                    error: function (xhr) {
+
+                    }
+                });
+            })
+        })
+
         $("#input-form").on("submit", function (event) {
             event.preventDefault();
             if (!validateForm()) {
@@ -93,32 +139,36 @@ $(function () {
             $.ajax({
                 url: "php/main.php",
                 type: "get",
+                async: true,
                 data: {
                     "xvalue": x,
                     "yvalue": y,
                     "rvalue": r
                 },
+                cache: false,
                 success: function (data) {
                     let moveX;
                     let moveY;
                     let snippet;
                     response = JSON.parse(data);
-                    snippet = 150 / response.rvalue;
-                    moveX = response.xvalue * snippet * response.rvalue / Math.abs(response.rvalue);
-                    moveY = -1 * response.yvalue * snippet * response.rvalue / Math.abs(response.rvalue);
-                    nextRow = "<tr>";
-                    nextRow += "<td>" + response.xvalue + "</td>";
-                    nextRow += "<td>" + response.yvalue + "</td>";
-                    nextRow += "<td>" + response.rvalue + "</td>";
-                    nextRow += "<td>" + response.currenttime + "</td>";
-                    nextRow += "<td>" + response.executiontime + "</td>";
-                    nextRow += "<td>" + response.hit + "</td>";
-                    nextRow += "</tr>";
-                    $("#result-table").append(nextRow);
-                    $('#pointer').animate({
-                        cx: 180 + moveX,
-                        cy: 180 + moveY
-                    }, 2000);
+                    if(response.valid) {
+                        snippet = 150 / response.rvalue;
+                        moveX = response.xvalue * snippet * response.rvalue / Math.abs(response.rvalue);
+                        moveY = -1 * response.yvalue * snippet * response.rvalue / Math.abs(response.rvalue);
+                        nextRow = "<tr>";
+                        nextRow += "<td>" + response.xvalue + "</td>";
+                        nextRow += "<td>" + response.yvalue + "</td>";
+                        nextRow += "<td>" + response.rvalue + "</td>";
+                        nextRow += "<td>" + response.currenttime + "</td>";
+                        nextRow += "<td>" + response.executiontime + "</td>";
+                        nextRow += "<td>" + response.hit + "</td>";
+                        nextRow += "</tr>";
+                        $("#result-table").append(nextRow);
+                        $('#pointer').animate({
+                            cx: 180 + moveX,
+                            cy: 180 + moveY
+                        }, 2000);
+                    } else alert("Unexpected error has occured");
                 },
                 error: function (jqXHR, exception) {
                     var msg = '';
--- php/main.php
+++ php/main.php
@@ -1,29 +1,29 @@
 <?php
 
-//function validateX($xValue) {
-//    return isset($xValue);
-//}
-//
-//function validateY($yValue) {
-//    $Y_MIN = -5;
-//    $Y_MAX = 3;
-//    return isset($yValue) && is_numeric($yValue) && ( $yValue > $Y_MIN ) && $yValue < $Y_MAX;
-//}
-//
-//function validateR($rValue) {
-//    return isset($rValue);
-//}
-//
-//function validateForm($xValue, $yValue, $rValue) {
-//    return validateR($rValue) && validateX($xValue) && validateY($yValue);
-//}
+function validateX($xValue) {
+    return isset($xValue) && is_numeric($xValue) && $xValue >= -3 && $xValue <= 5;
+}
+
+function validateY($yValue) {
+    $Y_MIN = -5;
+    $Y_MAX = 3;
+    return isset($yValue) && is_numeric($yValue) && ( $yValue > $Y_MIN ) && $yValue < $Y_MAX;
+}
+
+function validateR($rValue) {
+    return isset($rValue) && is_numeric($rValue) && $rValue >= 1 && $rValue <= 3;
+}
+
+function validateForm($xValue, $yValue, $rValue) {
+    return validateR($rValue) && validateX($xValue) && validateY($yValue);
+}
 
 function checkFirstQ($xValue, $yValue, $rValue) {
     return $xValue >= 0 && $xValue <= ($rValue-$yValue)/2 && $yValue <= $rValue;
 }
 
 function checkThirdQ($xValue, $yValue, $rValue) {
-    return $xValue <= 0 && abs($xValue) <= $rValue && abs($yValue) <= $rValue;
+    return $xValue <= 0 && abs($xValue) <= $rValue && abs($yValue) <= $rValue && $yValue <=0;
 }
 
 function checkFourthQ($xValue, $yValue, $rValue) {
@@ -34,6 +34,7 @@ function checkHit($xValue, $yValue, $rValue) {
     return checkFirstQ($xValue, $yValue, $rValue) || checkThirdQ($xValue, $yValue, $rValue) || checkFourthQ($xValue, $yValue, $rValue);
 }
 
+session_start();
 $start = microtime(true);
 date_default_timezone_set('Europe/Moscow');
 $currentTime = date('m/d/Y h:i:s a', time());
@@ -42,15 +43,15 @@ $xValue = floatval(htmlspecialchars($_GET['xvalue']));
 $yValue = floatval(htmlspecialchars($_GET['yvalue']));
 $rValue = floatval(htmlspecialchars($_GET['rvalue']));
 
-//$validation = validateForm($xValue, $yValue, $rValue);
+$validation = validateForm($xValue, $yValue, $rValue);
 $hit = checkHit($xValue, $yValue, $rValue);
 $responseHit = $hit ? 'true' : 'false';
-//$responseValidation = $validation ? 'true' : 'false';
+$responseValidation = $validation ? 'true' : 'false';
 
-//$executionTime = round($currentTime + microtime(true) - $start);
 $executionTime = number_format((float)microtime(true) - $start, 8, '.', '');
 
 $data = "{" .
+    "\"valid\":\"$responseValidation\"," .
     "\"xvalue\":\"$xValue\"," .
     "\"yvalue\":\"$yValue\"," .
     "\"rvalue\":\"$rValue\"," .
@@ -59,6 +60,11 @@ $data = "{" .
     "\"hit\":\"$responseHit\"" .
     "}";
 
+$result = array($xValue, $yValue, $rValue, $currentTime, $executionTime, $responseHit);
+if (!isset($_SESSION['results'])) {
+    $_SESSION['results'] = array();
+}
+array_push($_SESSION['results'], $result);
 //$jsonData = "{" . "\"Work\":\"worked\"" . "}";
 //$data = $xValue;
 
